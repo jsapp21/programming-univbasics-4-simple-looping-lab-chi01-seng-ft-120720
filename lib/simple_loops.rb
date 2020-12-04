7b5025cbc91472a8ca644822e3c21efad568108d
James Sapp <jameseisapp@gmail.com>
2020-12-04 20:36:40 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -46,11 +46,13 @@ end
 #   end 
 # end 
 
-def return_string_array(array)
-  count = 0
-  while count < array.length do
-    array[count] = array[count].to_s
-    count += 1
-  end
-  array
-end
+def 
+
+# def return_string_array(array)
+#   count = 0
+#   while count < array.length do
+#     array[count] = array[count].to_s
+#     count += 1
+#   end
+#   array
+# end
